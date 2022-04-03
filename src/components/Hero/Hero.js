dad534480f5a3a9ccc7a2be99625146b540b3205
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2022-04-03 23:45:07 +0530

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
       <SectionText>
       Welcome to my Personal Portfolio this website depicts all of the work that I have completed. It displays the growth and progress that I have made. I am excited to continue developing my skills and gaining valuable experiences.
       </SectionText>
-      <Button onClick={() => window.location = 'https://fizaayesha.github.io/projects-gallery.github.io/'}>Learn More</Button>
+      <Button onClick={() => window.location = 'https://blogs-by-ayesha.netlify.app/'}>Learn More</Button>
     </LeftSection>
   </Section>
 );
