017833dc511d4814b03ae8ec77373a3557d9a3b3
fizaayesha <fizaayesha696@gmail.com>
2022-05-08 00:26:49 +0530

    add

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,13 +1,41 @@
-import React from 'react';
-import { DiCoda, DiCodeBadge, DiCodeigniter, DiCss3, DiExtjs, DiFirebase, DiHtml5, DiJava, DiJavascript, DiMongodb, DiNodejs, DiReact, DiWebplatform, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import {
+  DiCoda,
+  DiCodeBadge,
+  DiCodeigniter,
+  DiCss3,
+  DiExtjs,
+  DiFirebase,
+  DiHtml5,
+  DiJava,
+  DiJavascript,
+  DiMongodb,
+  DiNodejs,
+  DiReact,
+  DiWebplatform,
+  DiZend,
+} from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
 const Technologies = () => (
   <Section id="tech">
     <SectionTitle>Skills</SectionTitle>
     <SectionText>
-     The capacity to learn is a gift, the ability to learn is a skill, the willingness to learn is a choice. So, here I listed all my technical skills that I have learnt yet
+      The capacity to learn is a gift, the ability to learn is a skill, the
+      willingness to learn is a choice. So, here I listed all my technical
+      skills that I have learnt yet
     </SectionText>
     <List>
       <ListItem>
@@ -19,12 +47,11 @@ const Technologies = () => (
             Java
             <br />
             <DiCodeBadge size="3rem" />
-
-            C          <br />
-
+            C <br />
             <DiCodeBadge size="3rem" />
-
-            C++
+            C++ <br />
+            <DiCodeBadge size="3rem" />
+            Python
           </ListParagraph>
         </ListContainer>
       </ListItem>
@@ -33,18 +60,20 @@ const Technologies = () => (
           <ListTitle>Web Technologies</ListTitle>
           <ListParagraph>
             <DiHtml5 size="3rem" />
-            HTML <br/>
+            HTML <br />
             <DiCss3 size="3rem" />
-            CSS <br/>
+            CSS <br />
             <DiJavascript size="3rem" />
             JavaScript
             <br />
             <DiNodejs size="3rem" />
-            Node.js <br/>
+            Node.js <br />
             <DiExtjs size="3rem" />
             Express.js <br />
             <DiMongodb size="3rem" />
-            Mongodb
+            Mongodb <br />
+            <DiReact size="3rem" />
+            ReactJs
           </ListParagraph>
         </ListContainer>
       </ListItem>
